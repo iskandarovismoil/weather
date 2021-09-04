4b581b1e41a9282ba047c160f382677aab3d022c
Ismoil <iskandarovismoilbek@gmail.com>
2021-09-04 09:07:50 +0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Info from './components/info';
+import Form from './components/form';
+import Weather from './components/weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = '2dcde87f83fe2233e9fa50e70c582c88';
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=2dcde87f83fe2233e9fa50e70c582c88&units=metric";
+
+class App extends React.Component{
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    date: undefined,
+    pressure: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+  getTime(time){
+    var date = new Date();
+    date.setTime(time);
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+  
+    if(city){
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await api_url.json();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        date: this.getTime(data.dt),
+        pressure: data.main.pressure,
+        sunrise: this.getTime(data.sys.sunrise),
+        sunset: this.getTime(data.sys.sunset),
+        error: ""
+      });
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <Info />
+        <Form weatherMethod={this.getWeather} />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          date={this.state.date}
+          pressure={this.state.pressure}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/info.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Info extends React.Component{
+
+    render(){
+
+        return(
+
+            <div>
+
+                <h2>One</h2>
+                <p>Hello</p>
+
+            </div>  
+
+        );
+
+    }
+
+}
+
+export default Info;
